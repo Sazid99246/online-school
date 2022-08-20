7e52aac934bdc84805cb6cd80bc2f544f2ac5cb0
Sheikh MD Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2022-08-20 07:09:33 +0600

    added firebase sdk and made created header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import Header from './Pages/Shared/Header';
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Login/Register';
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+      </Routes>
     </div>
   );
 }
